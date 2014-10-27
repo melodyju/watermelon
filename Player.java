c3d5b3a711a982e2256f9744d93fdeed025d6073
Melody Ju <melodyju@outlook.com>
2014-10-27 12:36:49 -0400

    Refactored stuff. Nothing functional is different.

--- Player.java
+++ Player.java
@@ -15,27 +15,180 @@ public class Player extends watermelon.sim.Player {
 	static double boardWidth;
 	static double boardHeight;
 	static double s;
+	static ArrayList<Pair> treeList;
 
-	private final int generationSize = 10;
-	private final int numGenerations = 3;
+	static int k;
+
+	private static final int generationSize = 20;
+	private static final int numGenerations = 5;
 
 	enum Region {
 		TOP, BOTTOM, LEFT, RIGHT
 	}
 
-	public void init() {
+	/////////////////////////////////////////////////////////////////////////////////////
+	// Generation and Individual classes
+
+	// Generation class 
+	class Generation {
+		public double totalFitness;
+		public ArrayList<Individual> population;
+
+		Generation(ArrayList<Individual> individuals) {
+			this.population = individuals;
+			totalFitness = calculateTotalFitness();
+		}
+
+		Generation(Generation parentGeneration) {
+			this.population = spawnChildren(parentGeneration);
+			totalFitness = calculateTotalFitness();
+		}
+
+		Generation() {
+			this.population = initFirstGeneration();
+			totalFitness = calculateTotalFitness();
+		}
+
+		Individual selectParent() {
+			return rouletteWheelSelection();
+		}
+
+		Individual rouletteWheelSelection() {
+			Random random = new Random();
+			double rnd = random.nextDouble() * this.totalFitness;
+			int j;
+			for (j = 0; j < generationSize && rnd > 0; j++) {
+				rnd -= population.get(j).fitness;
+			}
+			return population.get(j-1);
+		}
+
+		double calculateTotalFitness() {
+			double total = 0;
+			for (Individual individual : population) {
+				total += individual.fitness;
+			}
+			return total;
+		}
+
+		ArrayList<Individual> initFirstGeneration() {
+			ArrayList<Individual> individuals = new ArrayList<Individual>();
+			for (int i = 0; i < generationSize; i++) {
+				Individual individual = new Individual();
+				individuals.add(individual);
+			}
+			return individuals;
+		}
+
+		ArrayList<Individual> spawnChildren(Generation parentGeneration) {
+			ArrayList<Individual> children = new ArrayList<Individual>();
+
+			Individual parent1; //parent 1 of children[i]
+			Individual parent2; //parent 2 of children[i]
+			ArrayList<Individual> allChildren; //all children from these 2 parents
+			Individual fittestChild; //fittest child from these 2 parents -> children[i]
+
+			for (int i = 0; i < generationSize; i++) {
+				parent1 = parentGeneration.selectParent();
+				parent2 = parentGeneration.selectParent();
+				//parents reproduce 
+				allChildren = parent1.reproduce(parent2);
+				//add their fittest child to the list of children
+				fittestChild = fittestIndividual(allChildren);
+				children.add(i, fittestChild);
+			}
+
+			return children;
+		}
+
+		Individual fittestIndividual(ArrayList<Individual> population) {
+			double maxFitness = 0;
+			Individual fittestIndividual = null;
+			for (Individual individual : population) {
+				double fitness = individual.fitness;
+				if (fitness > maxFitness) {
+					maxFitness = fitness;
+					fittestIndividual = individual;
+				}
+			}
+			return fittestIndividual;
+		}
 
 	}
 
-	static double distance(seed tmp, Pair pair) {
-		return Math.sqrt((tmp.x - pair.x) * (tmp.x - pair.x) + (tmp.y - pair.y) * (tmp.y - pair.y));
+	// Individual class
+	class Individual {
+		public double fitness;
+		public ArrayList<seed> board;
+
+		Individual(ArrayList<seed> board) {
+			this.board = board;
+			fitness = calculateFitness();
+		}
+
+		Individual() {
+			this.board = generateRandomBoard();
+			fitness = calculateFitness();
+		}
+
+		double calculateFitness() {
+			double total = 0;
+			for (int i = 0; i < board.size(); i++) {
+				double score;
+				double chance = 0.0;
+				double totaldis = 0.0;
+				double difdis = 0.0;
+				for (int j = 0; j < board.size(); j++) {
+					if (j != i) {
+						totaldis = totaldis + Math.pow(distanceBetweenSeeds(board.get(i), board.get(j)), -2);
+					}
+				}
+				for (int j = 0; j < board.size(); j++) {
+					if (j != i && ((board.get(i).tetraploid && !board.get(j).tetraploid) || 
+						(!board.get(i).tetraploid && board.get(j).tetraploid))) {
+						difdis = difdis + Math.pow(distanceBetweenSeeds(board.get(i), board.get(j)), -2);
+					}
+				}
+				chance = difdis / totaldis;
+				score = chance + (1 - chance) * s;
+				total = total + score;
+			}
+			return total;
+		}
+
+		ArrayList<Individual> reproduce(Individual parent2) {
+			ArrayList<Individual> children = new ArrayList<Individual>();
+
+			ArrayList<seed> board1 = this.board;
+			ArrayList<seed> board2 = parent2.board;
+
+			ArrayList<seed> child1 = new ArrayList<seed>();
+			child1.addAll(getSeedsInRegion(board1, Region.TOP));
+			child1.addAll(getSeedsInRegion(board2, Region.BOTTOM));
+			
+			ArrayList<seed> child2 = new ArrayList<seed>();
+			child2.addAll(getSeedsInRegion(board1, Region.BOTTOM));
+			child2.addAll(getSeedsInRegion(board2, Region.TOP));
+			
+			ArrayList<seed> child3 = new ArrayList<seed>();
+			child3.addAll(getSeedsInRegion(board1, Region.LEFT));
+			child3.addAll(getSeedsInRegion(board2, Region.RIGHT));
+			
+			ArrayList<seed> child4 = new ArrayList<seed>();
+			child4.addAll(getSeedsInRegion(board1, Region.RIGHT));
+			child4.addAll(getSeedsInRegion(board2, Region.LEFT));
+			
+			children.add(new Individual(child1));
+			children.add(new Individual(child2));
+			children.add(new Individual(child3));
+			children.add(new Individual (child4));
+			
+			return children;
+		}
 	}
 
-	// Return: the next position
-	// my position: dogs[id-1]
-	static double distance(seed a, Point b) {
-		return Math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
-	}
+	/////////////////////////////////////////////////////////////////////////////////////
+	// Game stuff
 
 	@Override
 	public ArrayList<seed> move(ArrayList<Pair> treelist, double width, double length, double s) {
@@ -43,105 +196,49 @@ public class Player extends watermelon.sim.Player {
 		this.boardWidth = width;
 		this.boardHeight = length;
 		this.s = s;
+		this.treeList = treelist;
 
-		return bestProgeny(treelist, width, length, s);
+		Individual winner = runGeneticAlgorithm();
+		return winner.board;
 	}
 
-	public ArrayList<seed> bestProgeny(ArrayList<Pair> treelist, double width, double length, double s) {
-		//Initialize generation 0 (k = 0)
-		int k = 0;
-		ArrayList<ArrayList<seed>> parents = new ArrayList<ArrayList<seed>>();
-		for (int i = 0; i < generationSize; i++) {
-			parents.add(i, generateRandomBoard(treelist, width, length, s));
-		}
+	// Returns the final board from Genetic Algorithm 
+	public Individual runGeneticAlgorithm() {
+		Generation parents = null;
+		Generation children = null;
 
-		boolean condition = false;
-		ArrayList<ArrayList<seed>> children = null;
-		while (condition == false) {
+		// Initialize Generation 0 of random boards
+		k = 0;
+		parents = new Generation();
+
+		while (shouldTerminate() == false) {
 			System.out.println("Generation " + k);
-			//Create generation k + 1
-			children = spawnChildren(parents);
-			// System.out.println("Spawn children returned");
+			// Create generation k + 1
+			children = new Generation(parents);
 
 			k++;
-			if (k > numGenerations) {
-				condition = true;
-			}
+			// Children become the parents of the next generation
 			parents = children;
 		}
 
-		//now return the fittest person in the most current generation
-		return fittestIndividual(children);
+		// Return the fittest person in the most current generation
+		return children.fittestIndividual(children.population);
 	}
 
-	public ArrayList<seed> fittestIndividual(ArrayList<ArrayList<seed>> generation) {
-		double maxFitness = 0;
-		ArrayList<seed> fittestIndividual = null;
-		for (ArrayList<seed> individual : generation) {
-			double fitness = calculateFitness(individual);
-			if (fitness > maxFitness) {
-				maxFitness = fitness;
-				fittestIndividual = individual;
-			}
+	public boolean shouldTerminate() {
+		if (k > numGenerations) {
+			return true;
 		}
-		return fittestIndividual;
+		return false;
 	}
 
-	public ArrayList<ArrayList<seed>> spawnChildren(ArrayList<ArrayList<seed>> parents) {
-		ArrayList<ArrayList<seed>> children = new ArrayList<ArrayList<seed>>();
+	/////////////////////////////////////////////////////////////////////////////////////
+	// Helper methods
 
-		//calculate total fitness of parent generation
-		double[] parentFitness = new double[generationSize];
-		double totalFitness = 0;
-		for (int i = 0; i < generationSize; i++) {
-			double fitness = calculateFitness(parents.get(i));
-			parentFitness[i] = fitness;
-			totalFitness += fitness;
-		}
-
-		//fill in the child population
-		ArrayList<seed> parent1; //parent 1 of children[i]
-		ArrayList<seed> parent2; //parent 2 of children[i]
-		ArrayList<ArrayList<seed>> allChildren; //all children from these 2 parents
-		ArrayList<seed> fittestChild; //fittest child from these 2 parents -> children[i]
-
-		for (int i = 0; i < generationSize; i++) {
-			//select first parent by roulette wheel selection
-			parent1 = rouletteWheelSelection(parents, parentFitness, totalFitness);
-			//select second parent by roulette wheel selection
-			parent2 = rouletteWheelSelection(parents, parentFitness, totalFitness);
-			//parents reproduce 
-			allChildren = getChildrenFromParentBoards(parent1, parent2);
-			//add their fittest child to the list of children
-			fittestChild = fittestIndividual(allChildren);
-			children.add(i, fittestChild);
-		}
-
-		return children;
-	}
-
-	public double totalFitness(ArrayList<ArrayList<seed>> generation) {
-		double totalFitness = 0;
-		for (ArrayList<seed> individual : generation) {
-			totalFitness += calculateFitness(individual);
-		}
-		return totalFitness;
-	}
-
-	public ArrayList<seed> rouletteWheelSelection(ArrayList<ArrayList<seed>> parents, double[] parentFitness, double totalFitness) {
-		Random random = new Random();
-		double rnd = random.nextDouble() * totalFitness;
-		int j;
-		for (j = 0; j < generationSize && rnd > 0; j++) {
-			rnd -= parentFitness[j];
-		}
-		return parents.get(j-1);
-	}
-
-	public ArrayList<seed> generateRandomBoard(ArrayList<Pair> treelist, double width, double length, double s) {
+	public ArrayList<seed> generateRandomBoard() {
 		ArrayList<seed> seedlist = new ArrayList<seed>();
-		for (double i = distowall; i < width - distowall; i = i + distoseed) {
-			for (double j = distowall; j < length - distowall; j = j + distoseed) {
+		for (double i = distowall; i < boardWidth - distowall; i = i + distoseed) {
+			for (double j = distowall; j < boardHeight - distowall; j = j + distoseed) {
 				Random random = new Random();
 				seed tmp;
 				if (random.nextInt(2) == 0)
@@ -149,8 +246,8 @@ public class Player extends watermelon.sim.Player {
 				else
 					tmp = new seed(i, j, true);
 				boolean add = true;
-				for (int f = 0; f < treelist.size(); f++) {
-					if (distance(tmp, treelist.get(f)) < distotree) {
+				for (int f = 0; f < treeList.size(); f++) {
+					if (distance(tmp, treeList.get(f)) < distotree) {
 						add = false;
 						break;
 					}
@@ -170,16 +267,27 @@ public class Player extends watermelon.sim.Player {
 		for (seed position : positions) {
 			double x = position.x;
 			double y = position.y;
-			int identity = random.nextInt(3);
+			// int identity = random.nextInt(3);
+			// switch (identity) {
+			// 	case 0:
+			// 		//don't fill this position 
+			// 		break;
+			// 	case 1:
+			// 		seedlist.add(new seed(x, y, true));
+			// 		break;
+			// 	case 2:
+			// 		seedlist.add(new seed(x, y, false));
+			// 		break;
+			// 	default:
+			// 		break;
+			// }
+			int identity = random.nextInt(2);
 			switch (identity) {
 				case 0:
-					//don't fill this position 
+					seedlist.add(new seed(x, y, false));
 					break;
 				case 1:
 					seedlist.add(new seed(x, y, true));
-					break;
-				case 2:
-					seedlist.add(new seed(x, y, false));
 					break;
 				default:
 					break;
@@ -187,55 +295,28 @@ public class Player extends watermelon.sim.Player {
 		}
 		return seedlist;
 	}
-
-	public ArrayList<ArrayList<seed>> getChildrenFromParentBoards(ArrayList<seed> board1, ArrayList<seed> board2) {
-		ArrayList<ArrayList<seed>> childrenBoards = new ArrayList<ArrayList<seed>>();
-		
-		ArrayList<seed> child1 = new ArrayList<seed>();
-		child1.addAll(getSeedsInRegion(board1, Region.TOP));
-		child1.addAll(getSeedsInRegion(board2, Region.BOTTOM));
-		
-		ArrayList<seed> child2 = new ArrayList<seed>();
-		child2.addAll(getSeedsInRegion(board1, Region.BOTTOM));
-		child2.addAll(getSeedsInRegion(board2, Region.TOP));
-		
-		ArrayList<seed> child3 = new ArrayList<seed>();
-		child3.addAll(getSeedsInRegion(board1, Region.LEFT));
-		child3.addAll(getSeedsInRegion(board2, Region.RIGHT));
-		
-		ArrayList<seed> child4 = new ArrayList<seed>();
-		child4.addAll(getSeedsInRegion(board1, Region.RIGHT));
-		child4.addAll(getSeedsInRegion(board2, Region.LEFT));
-		
-		childrenBoards.add(child1);
-		childrenBoards.add(child2);
-		childrenBoards.add(child3);
-		childrenBoards.add(child4);
-		
-		return childrenBoards;
-	}
 	
 	public ArrayList<seed> getSeedsInRegion(ArrayList<seed> board, Region region) {
 		ArrayList<seed> seedsInRegion = new ArrayList<seed>();
 		for (seed nextSeed : board) {
 			switch(region) {
 				case TOP:
-					if (nextSeed.y <= (this.boardHeight - SEED_RADIUS) / 2.0) {
+					if (nextSeed.y <= (this.boardHeight) / 2.0) {
 						seedsInRegion.add(nextSeed);
 					}
 					break;
 				case BOTTOM:
-					if (nextSeed.y >= (this.boardHeight + SEED_RADIUS) / 2.0) {
+					if (nextSeed.y >= (this.boardHeight) / 2.0) {
 						seedsInRegion.add(nextSeed);
 					}
 					break;
 				case LEFT:
-					if (nextSeed.x <= (this.boardWidth - SEED_RADIUS) / 2.0) {
+					if (nextSeed.x <= (this.boardWidth) / 2.0) {
 						seedsInRegion.add(nextSeed);
 					}
 					break;
 				case RIGHT:
-					if (nextSeed.x >= (this.boardWidth + SEED_RADIUS) / 2.0) {
+					if (nextSeed.x >= (this.boardWidth) / 2.0) {
 						seedsInRegion.add(nextSeed);
 					}
 					break;
@@ -247,40 +328,20 @@ public class Player extends watermelon.sim.Player {
 		}
 		return seedsInRegion;
 	}
-
-	public double calculateFitness(ArrayList<seed> seedlist) {
-		double total = 0;
-		for (int i = 0; i < seedlist.size(); i++) {
-			double score;
-			double chance = 0.0;
-			double totaldis = 0.0;
-			double difdis = 0.0;
-			for (int j = 0; j < seedlist.size(); j++) {
-				if (j != i) {
-					totaldis = totaldis
-							+ Math.pow(
-									distanceseed(seedlist.get(i),
-											seedlist.get(j)), -2);
-				}
-			}
-			for (int j = 0; j < seedlist.size(); j++) {
-				if (j != i
-						&& ((seedlist.get(i).tetraploid && !seedlist.get(j).tetraploid) || (!seedlist
-								.get(i).tetraploid && seedlist.get(j).tetraploid))) {
-					difdis = difdis
-							+ Math.pow(
-									distanceseed(seedlist.get(i),
-											seedlist.get(j)), -2);
-				}
-			}
-			chance = difdis / totaldis;
-			score = chance + (1 - chance) * s;
-			total = total + score;
-		}
-		return total;
-	}
 	
-	static double distanceseed(seed a, seed b) {
+	static double distanceBetweenSeeds(seed a, seed b) {
+		return Math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
+	}
+
+	public void init() {
+
+	}
+
+	static double distance(seed tmp, Pair pair) {
+		return Math.sqrt((tmp.x - pair.x) * (tmp.x - pair.x) + (tmp.y - pair.y) * (tmp.y - pair.y));
+	}
+
+	static double distance(seed a, Point b) {
 		return Math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
 	}
 
