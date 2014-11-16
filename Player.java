41502f09f9d3990ebabe40a8f835adea27a2beb2
Qiurui Jin <jqiurui@gmail.com>
2014-11-16 13:09:30 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,32 +1,41 @@
-package watermelon.group2;
+package watermelon.group8;
 
 import java.util.*;
 
+import watermelon.group2.Packing;
+import watermelon.group2.Position;
 import watermelon.sim.Pair;
 import watermelon.sim.Point;
 import watermelon.sim.seed;
 
 public class Player extends watermelon.sim.Player {
-	// static double distowall = 2.1;
-	// static double distotree = 2.2;
-	// static double distoseed = 1.01;
-	static double distToWall= 1.0;
-	static double distToSeed = 2.000000000000000000001;
-	static double distToTree = 2.000000000000000000001;
+	static double distowall = 2.1;
+	static double distotree = 2.2;
+	static double distoseed = 2.01;
 	static double SEED_RADIUS = 1.0;
-	static double MARGIN = 2.0;
-	static int MUTATION_PROBABILITY = 5; // 1 in every *MUTATION_PROBABILITY* seeds within the margin gets mutated
+	static double distToWall= 1.0;
+	static double distToSeed = 2.0000000000000001;
+	static double distToTree = 2.0000000000000001;
+	static ArrayList<seed> globalBoard;
+
 	static double boardWidth;
 	static double boardHeight;
 	static double s;
 	static ArrayList<Pair> treeList;
+
 	static int k;
 	static Individual best;
-	private static final int generationSize = 20;
-	private static final int numGenerations = 20;
-	private static final int childPolicy = 50;
-	private static final int mutationProbability = 5; // 1 in *mutationProbability* boundary seeds will be flipped
+
+	private static final int generationSize = 4;
+	private static final int numGenerations = 30;
+	private static final int childPolicy = 15;
+
+	enum Region {
+		TOP, BOTTOM, LEFT, RIGHT
+	}
+
 	/////////////////////////////////////////////////////////////////////////////////////
+	// Generation and Individual classes
 	static boolean closeToTree(Pair p, ArrayList<Pair> treeLst){
 		for(Pair q : treeLst){
 			if(Math.sqrt((p.x - q.x) * (p.x - q.x) + (p.y - q.y) * (p.y - q.y)) < distToTree){
@@ -35,28 +44,31 @@ public class Player extends watermelon.sim.Player {
 		}
 		return false;
 	}
-
-	// Generation and Individual classes
 	// Generation class 
 	class Generation {
 		public double totalFitness;
 		public ArrayList<Individual> population;
+
 		Generation(ArrayList<Individual> individuals) {
 			this.population = individuals;
 			totalFitness = calculateTotalFitness();
 		}
+
 		Generation(Generation parentGeneration) {
 			this.population = spawnChildren(parentGeneration);
 			totalFitness = calculateTotalFitness();
 		}
+
 		Generation() {
 			this.population = initFirstGeneration();
 			totalFitness = calculateTotalFitness();
 		}
+
 		Individual selectParent() {
 			// return rouletteWheelSelection();
 			return probabilisticSelection();
 		}
+
 		Individual rouletteWheelSelection() {
 			Random random = new Random();
 			double rnd = random.nextDouble() * this.totalFitness;
@@ -66,6 +78,7 @@ public class Player extends watermelon.sim.Player {
 			}
 			return population.get(j-1);
 		}
+
 		Individual probabilisticSelection() {
 			Individual chosenOne = null;
 			Random random = new Random();
@@ -78,6 +91,7 @@ public class Player extends watermelon.sim.Player {
 			}
 			return chosenOne;
 		}
+
 		double calculateTotalFitness() {
 			double total = 0;
 			for (Individual individual : population) {
@@ -85,6 +99,7 @@ public class Player extends watermelon.sim.Player {
 			}
 			return total;
 		}
+
 		ArrayList<Individual> initFirstGeneration() {
 			ArrayList<Individual> individuals = new ArrayList<Individual>();
 			for (int i = 0; i < generationSize; i++) {
@@ -93,12 +108,15 @@ public class Player extends watermelon.sim.Player {
 			}
 			return individuals;
 		}
+
 		ArrayList<Individual> spawnChildren(Generation parentGeneration) {
 			ArrayList<Individual> children = new ArrayList<Individual>();
+
 			Individual parent1; //parent 1 of children[i]
 			Individual parent2; //parent 2 of children[i]
 			ArrayList<Individual> allChildren; //all children from these 2 parents
 			Individual fittestChild; //fittest child from these 2 parents -> children[i]
+
 			for (int i = 0; i < generationSize; i++) {
 				parent1 = parentGeneration.selectParent();
 				parent2 = parentGeneration.selectParent();
@@ -108,8 +126,10 @@ public class Player extends watermelon.sim.Player {
 				fittestChild = fittestIndividual(allChildren);
 				children.add(i, fittestChild);
 			}
+
 			return children;
 		}
+
 		Individual fittestIndividual(ArrayList<Individual> population) {
 			double maxFitness = 0;
 			Individual fittestIndividual = null;
@@ -122,19 +142,25 @@ public class Player extends watermelon.sim.Player {
 			}
 			return fittestIndividual;
 		}
+
 	}
+
 	// Individual class
 	class Individual {
 		public double fitness;
 		public ArrayList<seed> board;
+
+		
 		Individual(ArrayList<seed> board) {
 			this.board = board;
 			fitness = calculateFitness();
 		}
+
 		Individual() {
 			this.board = generateRandomBoard();
 			fitness = calculateFitness();
 		}
+
 		double calculateFitness() {
 			double total = 0;
 			for (int i = 0; i < board.size(); i++) {
@@ -149,7 +175,7 @@ public class Player extends watermelon.sim.Player {
 				}
 				for (int j = 0; j < board.size(); j++) {
 					if (j != i && ((board.get(i).tetraploid && !board.get(j).tetraploid) || 
-							(!board.get(i).tetraploid && board.get(j).tetraploid))) {
+						(!board.get(i).tetraploid && board.get(j).tetraploid))) {
 						difdis = difdis + Math.pow(distanceBetweenSeeds(board.get(i), board.get(j)), -2);
 					}
 				}
@@ -159,11 +185,20 @@ public class Player extends watermelon.sim.Player {
 			}
 			return total;
 		}
+
+		// void mutate(Individual parent1, Individual parent2) {
+		// 	Random random = new Random();
+		// 	int patches = 
+		// }
+
 		ArrayList<Individual> reproduce(Individual parent2) {
 			ArrayList<Individual> children = new ArrayList<Individual>();
+
 			ArrayList<seed> board1 = this.board;
 			ArrayList<seed> board2 = parent2.board;
+
 			Random random = new Random();
+
 			for (int i = 0; i < childPolicy; i++) {
 				//randomly select which parent is 1 and which parent is 2
 				int swap = random.nextInt(2);
@@ -171,34 +206,20 @@ public class Player extends watermelon.sim.Player {
 					board1 = parent2.board;
 					board2 = this.board;
 				}
+
 				int xBoundary = random.nextInt((int)boardWidth);
 				int yBoundary = random.nextInt((int)boardHeight);
 				ArrayList<seed> board = new ArrayList<seed>();
 				board.addAll(getSeedsInRegion(board1, 0, xBoundary, 0, yBoundary));
 				board.addAll(getSeedsInRegion(board2, xBoundary, boardWidth, 0, boardHeight));
 				board.addAll(getSeedsInRegion(board2, 0, xBoundary, yBoundary, boardHeight));
-				// Add mutations?
-				// Mutations along boundaries 
-				// board = mutate(board, xBoundary, yBoundary);
+
 				children.add(new Individual(board));
 			}
-
+			
 			return children;
 		}
-		// ArrayList<seed> mutate(ArrayList<seed> board, int xBoundary, int yBoundary) {
-		// 	Random random = new Random();
-		// 	int ticket;
-		// 	for (seed s : board) {
-		// 		if (Math.abs(s.x - xBoundary) <= 3 || Math.abs(s.y - yBoundary) <= 3) {
-		// 			// Seed near boundary
-		// 			ticket = random.nextInt(mutationProbability + 1);
-		// 			if (ticket == mutationProbability) {
-		// 				s.tetraploid = !(s.tetraploid);
-		// 			}
-		// 		}
-		// 	}
-		// 	return board;
-		// }
+
 		boolean chosen(double totalFitness) {
 			double proportion = this.fitness / totalFitness;
 			Random random = new Random();
@@ -211,8 +232,10 @@ public class Player extends watermelon.sim.Player {
 			}
 		}
 	}
+
 	/////////////////////////////////////////////////////////////////////////////////////
 	// Game stuff
+
 	@Override
 	public ArrayList<seed> move(ArrayList<Pair> treelist, double width, double length, double s) {
 		// TODO Auto-generated method stub
@@ -220,21 +243,28 @@ public class Player extends watermelon.sim.Player {
 		this.boardHeight = length;
 		this.s = s;
 		this.treeList = treelist;
+
 		this.best = new Individual();
+
 		Individual winner = runGeneticAlgorithm();
 		return winner.board;
 	}
+
 	// Returns the final board from Genetic Algorithm 
 	public Individual runGeneticAlgorithm() {
 		Generation parents = null;
 		Generation children = null;
+
 		// Initialize Generation 0 of random boards
 		k = 0;
 		parents = new Generation();
+
 		while (shouldTerminate() == false) {
 			System.out.println("Generation " + k);
+			System.out.println(parents.totalFitness);
 			// Create generation k + 1
 			children = new Generation(parents);
+
 			k++;
 			// Check to see if the fittest of this generation is fitter than the GOAT
 			Individual fittestChild = children.fittestIndividual(children.population);
@@ -244,65 +274,68 @@ public class Player extends watermelon.sim.Player {
 			// Children become the parents of the next generation
 			parents = children;
 		}
+
 		// Return the fittest person in the most current generation
 		return best;
 	}
+
 	public boolean shouldTerminate() {
 		if (k > numGenerations) {
 			return true;
 		}
 		return false;
 	}
+
 	/////////////////////////////////////////////////////////////////////////////////////
 	// Helper methods
-	public ArrayList<seed> generateRandomBoard() {
-		ArrayList<seed> seedlist = new ArrayList<seed>();
-		ArrayList<Position> trees = new ArrayList<Position>();
 
-		for (Pair p : treeList)
-			trees.add(new Position(p.x, p.y));
+		public ArrayList<seed> generateRandomBoard() {
+			ArrayList<seed> seedlist = new ArrayList<seed>();
+			ArrayList<Position> trees = new ArrayList<Position>();
 
-		ArrayList<Position> locations = new ArrayList<Position>();
+			for (Pair p : treeList)
+				trees.add(new Position(p.x, p.y));
 
-		if (trees.size() > 0) {
-			for (int i = 0; i < 1000; i++) {
-				ArrayList<Position> workingLocations = new ArrayList<Position>();
-				Packing.treeExpansion(trees, null, boardWidth, boardHeight, workingLocations);
-				if (workingLocations.size() > locations.size()) {
-					locations = new ArrayList<Position>(workingLocations);
+			ArrayList<Position> locations = new ArrayList<Position>();
+
+			if (trees.size() > 0) {
+				for (int i = 0; i < 1000; i++) {
+					ArrayList<Position> workingLocations = new ArrayList<Position>();
+					Packing.treeExpansion(trees, null, boardWidth, boardHeight, workingLocations);
+					if (workingLocations.size() > locations.size()) {
+						locations = new ArrayList<Position>(workingLocations);
+					}
 				}
+
+
+				Packing.treeExpansionBreadthFirst(trees, null, boardWidth, boardHeight, locations);
+
+				Packing.pullToTrees(locations, trees, boardWidth, boardHeight);
+				fillInSpace(trees,boardWidth,boardHeight,locations);	
+
+				ArrayList<Position> hexLocations = Packing.hexagonal(trees, boardWidth, boardHeight);
+				Packing.treeExpansionBreadthFirst(trees, null, boardWidth, boardHeight, hexLocations);
+				Packing.pullToTrees(hexLocations, trees, boardWidth, boardHeight);
+				fillInSpace(trees, boardWidth, boardHeight, hexLocations);
+				if (hexLocations.size() > locations.size()) {
+	 			System.out.println("hex packing chosen");
+					locations = hexLocations;
+				}
+
+				//ArrayList<Position> locations = Packing.hexagonal(trees, boardWidth, boardHeight);
+				System.out.println("Board has " + locations.size() + " seeds.");
 			}
 
-
-			Packing.treeExpansionBreadthFirst(trees, null, boardWidth, boardHeight, locations);
-
-			Packing.pullToTrees(locations, trees, boardWidth, boardHeight);
-			fillInSpace(trees,boardWidth,boardHeight,locations);	
-
-			ArrayList<Position> hexLocations = Packing.hexagonal(trees, boardWidth, boardHeight);
-			Packing.treeExpansionBreadthFirst(trees, null, boardWidth, boardHeight, hexLocations);
-			Packing.pullToTrees(hexLocations, trees, boardWidth, boardHeight);
-			fillInSpace(trees, boardWidth, boardHeight, hexLocations);
-			if (hexLocations.size() > locations.size()) {
-				System.out.println("hex packing chosen");
-				locations = hexLocations;
+			else {
+				locations = Packing.hexagonal(trees, boardWidth, boardHeight);
+				fillInSpace(trees,boardWidth,boardHeight,locations);
+				System.out.println("Board has " + locations.size() + " seeds.");
 			}
 
-			//ArrayList<Position> locations = Packing.hexagonal(trees, boardWidth, boardHeight);
-			System.out.println("Board has " + locations.size() + " seeds.");
+			
+			return generateRandomBoardFromPositions(locations);
 		}
-
-		else {
-			locations = Packing.hexagonal(trees, boardWidth, boardHeight);
-			fillInSpace(trees,boardWidth,boardHeight,locations);
-			System.out.println("Board has " + locations.size() + " seeds.");
-		}
-
-		
-		return generateRandomBoardFromPositions(locations);
-	}
-
-
+	
 	public static void fillInSpace(ArrayList<Position> trees, double width, double height, ArrayList<Position> board)
 	{
 		Position nextSeed = getNextAvailableSeed(board, treeList, 1,width,height);
@@ -491,6 +524,30 @@ public class Player extends watermelon.sim.Player {
 	}
 
 
+	public static ArrayList<Position> hexagonal(ArrayList<Position> trees, double width, double height) {
+		double x =  SEED_RADIUS;
+		double y =  SEED_RADIUS;
+		boolean offset = false;
+
+		ArrayList<Position> locations = new ArrayList<Position>();
+
+		while (y <= height -  SEED_RADIUS) {
+			x =  SEED_RADIUS + (offset ?  SEED_RADIUS : 0);
+
+			while (x <= width -  SEED_RADIUS) {
+					locations.add(new Position(x, y));
+
+				x += 2* SEED_RADIUS;
+			}
+
+			y +=  1.732051* SEED_RADIUS;
+			offset = !offset;
+		}
+
+		return locations;
+	}
+
+	
 	public ArrayList<seed> generateRandomBoardFromPositions(ArrayList<Position> positions) {
 		ArrayList<seed> seedlist = new ArrayList<seed>();
 		Random random = new Random();
@@ -513,50 +570,40 @@ public class Player extends watermelon.sim.Player {
 			// }
 			int identity = random.nextInt(2);
 			switch (identity) {
-			case 0:
-				seedlist.add(new seed(x, y, false));
-				break;
-			case 1:
-				seedlist.add(new seed(x, y, true));
-				break;
-			default:
-				break;
+				case 0:
+					seedlist.add(new seed(x, y, false));
+					break;
+				case 1:
+					seedlist.add(new seed(x, y, true));
+					break;
+				default:
+					break;
 			}
 		}
 		return seedlist;
 	}
-
+	
 	public ArrayList<seed> getSeedsInRegion(ArrayList<seed> board, double minX, double maxX, double minY, double maxY) {
 		ArrayList<seed> seedsInRegion = new ArrayList<seed>();
 		for (seed nextSeed : board) {
-			double x = nextSeed.x;
-			double y = nextSeed.y;
-			if (x >= minX + SEED_RADIUS && x < maxX - SEED_RADIUS && y >= minY + SEED_RADIUS && y < maxY - SEED_RADIUS) {
-				// Mutate seeds close to boundaries
-				// if (Math.abs(x - minX) <= MARGIN || Math.abs(x - maxX) <= MARGIN || 
-				// 	Math.abs(y - minY) <= MARGIN || Math.abs(y - maxY) <= MARGIN) {
-				// 	if (mutate()) {
-				// 		nextSeed.tetraploid = !nextSeed.tetraploid;
-				// 	}
-				// }
+			if (nextSeed.x >= minX && nextSeed.x < maxX && nextSeed.y >= minY && nextSeed.y < maxY) {
 				seedsInRegion.add(nextSeed);
 			}
 		}
 		return seedsInRegion;
 	}
-
-	static boolean mutate() {
-		Random random = new Random();
-		if (random.nextInt(MUTATION_PROBABILITY + 1) == MUTATION_PROBABILITY) {
-			return true;
-		}
-		return false;
-	}
-
+	
+	
 	static double distanceBetweenSeeds(seed a, seed b) {
 		return Math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
 	}
+
 	public void init() {
+         
+	}
+
+	static double distance(seed tmp, Pair pair) {
+		return Math.sqrt((tmp.x - pair.x) * (tmp.x - pair.x) + (tmp.y - pair.y) * (tmp.y - pair.y));
 	}
 	static double distance(Position tmp, Pair pair) {
 		return Math.sqrt((tmp.x - pair.x) * (tmp.x - pair.x) + (tmp.y - pair.y) * (tmp.y - pair.y));
@@ -564,4 +611,5 @@ public class Player extends watermelon.sim.Player {
 	static double distance(seed a, Point b) {
 		return Math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
 	}
+
 }
